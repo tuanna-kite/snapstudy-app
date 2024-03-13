1022f3d2e92ee4d9db2e769f9cd7a007e7fe57ef
Quang Akiyama <quanganhcf2@gmail.com>
2024-03-13 20:14:51 +0700

    create folder img, components, gitignore file

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <nav class="flex flex-row">
+        <div>
+            <img src="img/logo.png" alt="">
+        </div>
+        <div>
+            <button class="p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none" onclick="handleClick()">
+                icon
+            </button>
+        </div>
+    </nav>
+</div>

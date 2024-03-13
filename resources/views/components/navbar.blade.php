515c5f9935d376e451942b30fd229b8059728fea
Quang Akiyama <quanganhcf2@gmail.com>
2024-03-13 20:44:29 +0700

    navbar UI

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,15 @@
-<div class="container">
-    <nav class="flex flex-row">
+<nav class="fixed top-0 left-0 right-0">
+    <div class="container mx-auto flex justify-between items-center">
         <div>
             <img src="img/logo.png" alt="">
         </div>
-        <div>
-            <button class="p-2 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none" onclick="handleClick()">
-                icon
+        <div class="flex items-center">
+            <button class="p-2 rounded-full transparent focus:outline-none" onclick="handleClick()">
+                <img src="img/cart.svg" alt="" width="24" height="24">
             </button>
+            <button class="p-2 rounded-full transparent focus:outline-none" onclick="handleClick()">
+              <img src="img/avatar.png" alt="" width="48" height="48">
+          </button>
         </div>
-    </nav>
-</div>
+    </div>
+</nav>

d719762f110bbf9881a202d27d13de89afe81e9f
Quang Akiyama <quanganhcf2@gmail.com>
2024-03-14 18:30:17 +0700

    Footer UI

--- resources/views/components/documents/document-grid.blade.php
+++ resources/views/components/documents/document-grid.blade.php
@@ -1,5 +1,5 @@
 <div class="flex justify-center">
-    <div class="grid gap-6 md:grid-cols-4 md:grid-rows-1 sm:grid-cols-2 sm:grid-rows-1" style="max-width: 1200px">
+    <div class="w-full grid gap-8 lg:gap-4 lg:grid-cols-4 lg:grid-rows-1 sm:grid-cols-2 sm:grid-rows-1" style="max-width: 1200px">
         <x-documents.document-card/>
         <x-documents.document-card/>
         <x-documents.document-card/>

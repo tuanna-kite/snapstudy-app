a788fbf66c80894d98daf7ebbdf04adf023b628d
Quang Akiyama <quanganhcf2@gmail.com>
2024-03-14 10:54:18 +0700

    slide section

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,9 @@
+@props(['name'])
+
+
+@if (file_exists("img/icon/$name.svg"))
+    <img src="{{ 'img/icon/' . $name . '.svg' }}" alt="cart"
+        {{ $attributes->merge(['width' => 24, 'height' => 24]) }}>
+@else
+    <img alt="no-icon">
+@endif
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,18 @@
-<nav class="fixed top-0 left-0 right-0">
+<nav class="fixed top-0 left-0 right-0 shadow p-1" style="height:80px">
     <div class="container mx-auto flex justify-between items-center">
-        <div>
-            <img src="img/logo.png" alt="">
-        </div>
+        <a href="#">
+            <img src="img/logo.png" alt="Logo">
+        </a>
         <div class="flex items-center">
             <button class="p-2 rounded-full transparent focus:outline-none" onclick="handleClick()">
-                <img src="img/cart.svg" alt="" width="24" height="24">
+                <x-icon name='cart' />
             </button>
             <button class="p-2 rounded-full transparent focus:outline-none" onclick="handleClick()">
-              <img src="img/avatar.png" alt="" width="48" height="48">
-          </button>
+                <x-icon name='notifications' />
+            </button>
+            <button class="p-2 rounded-full transparent focus:outline-none" onclick="handleClick()">
+                <img src="img/avatar.png" alt="" width="48" height="48">
+            </button>
         </div>
     </div>
 </nav>

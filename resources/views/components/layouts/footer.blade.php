af6e6b6e19e09c963519faf86e17dc4854fa71cc
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-03-14 13:08:01 +0700

    add secondary color and create banner footer

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <section id="footer-banner" class="w-full flex justify-center">
-    <div class="container relative flex-1 flex justify-between flex-col md:flex-row items-center rounded-2xl p-10 bg-primary.light"
+    <div class="py-8 md:py-0 relative flex-1 flex justify-between flex-col md:flex-row items-center rounded-2xl px-10 bg-primary.light"
          style="max-width: 1200px">
-        <div class="flex flex-col md:w-3/5 sm:w-full">
+        <div class="flex flex-col md:w-3/5 w-full">
             <h3 class="text-primary.main text-2xl font-semibold">
                 Get ahead to achieve your academic goals with SNAPS
             </h3>
@@ -17,7 +17,7 @@
             </a>
         </div>
         <div class="w-full mt-5 md:mt-0 md:w-fit flex justify-end">
-            <img class="w-9/12" style="height: 100%"
+            <img class="w-9/12 md:w-full" style="max-width: 360px; height: 100%"
                  src="{{asset('assets/default/image/footer-3d.png')}}"
                  alt="footer-3d"/>
         </div>
